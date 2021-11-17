2fe18337bb24850c4c996b8ce7e95bda64221048
Максим С <dvdvdvd90@mail.ru>
2021-11-17 06:28:36 +0000

    Revert "В работе"
    This reverts commit 774e4102964b51a9314418aa9a2350ce408959d9

--- 02_IntroductionToDjango/project/board/advertisement/views.py
+++ 02_IntroductionToDjango/project/board/advertisement/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def advertisement_list(requset, *args, **kwargs):
--- 04_DatabasesAndModels/board/advertisements_app/admin.py
+++ 04_DatabasesAndModels/board/advertisements_app/admin.py
@@ -1,17 +1,3 @@
 from django.contrib import admin
-from .models import Advertisement, Author, Category
 
-
-@admin.register(Advertisement)
-class AdvertisementAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Author)
-class AdvertisementAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Category)
-class AdvertisementAdmin(admin.ModelAdmin):
-    pass
+# Register your models here.
