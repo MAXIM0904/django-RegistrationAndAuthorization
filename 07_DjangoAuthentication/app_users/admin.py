298fceb92d2590e1341d57e51c19ad1e4c0ca34e
Max <dvdvdvd90@mail.ru>
2021-12-05 19:19:47 +0300

    Выполнил

--- 07_DjangoAuthentication/app_users/admin.py
+++ 07_DjangoAuthentication/app_users/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import News, Comment
+from django.template.defaultfilters import truncatechars
 
-# Register your models here.
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_news', 'status']
+    list_filter = ('status',)
+    search_fields = ('name',)
+    inlines = [CommentInLine]
+    list_editable = ('status',)
+
+    actions = ['mark_as_actions', 'mark_as_inactive']
+
+    def mark_as_actions(self, request, queryset):
+        queryset.update(status="y")
+
+    def mark_as_inactive(self, request, queryset):
+        queryset.update(status="n")
+
+    mark_as_inactive.short_description = 'Перевести в статус неактивно'
+    mark_as_actions.short_description = 'Перевести в статус активно'
+
+    def __str__(self):
+        return "Новости"
+
+class CommentAdmin(admin.ModelAdmin):
+
+    def short_description(self):
+        return truncatechars(self.text_comment, 15)
+
+    short_description.short_description = 'Текст комментария'
+
+    list_display = ['id_news', 'author', short_description]
+    list_filter = ('author',)
+    search_fields = ('author', 'text_comment',)
+
+
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Comment, CommentAdmin)
--- 07_DjangoAuthentication/app_users/views.py
+++ 07_DjangoAuthentication/app_users/views.py
@@ -1,3 +1,88 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-# Create your views here.
+from django.views.generic.edit import FormMixin
+from .forms import CommentForm
+from .models import News, Comment
+
+
+def list_news(requset, *args, **kwargs):
+    return render(requset, 'news/list_news.html', {})
+
+def aut_str(requset, *args, **kwargs):
+    return render(requset, 'news/login_page.html', {})
+
+class NewsListView(ListView):
+    model = News
+    template_name = 'news/news_list.html'
+
+
+class NewsDetailView(DetailView, FormMixin):
+    model = News
+    template_name = 'news/news_detail.html'
+    form_class = CommentForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy("news_detail", kwargs={"pk": self.get_object().id})
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        user_form_news = form.save(commit=False)
+        user_form_news.id_news = self.get_object()
+        try:
+            user_form_news.author = self.request.user
+        except:
+            user_form_news.name = f"Аноним {self.request.POST['name']}"
+        user_form_news.save()
+        return super().form_valid(form)
+
+
+class NewsCreateView(CreateView):
+    model = News
+    template_name = 'news/new_news.html'
+    fields = '__all__'
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('list_news')
+
+
+class NewsUpdateView(UpdateView):
+    model = News
+    template_name = 'news/change.html'
+    fields = '__all__'
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('list_news')
+
+
+class CommentListView(ListView):
+    model = Comment
+    template_name = 'news/comment_list.html'
+
+
+class CommentDetailView(DetailView):
+    model = Comment
+    template_name = 'news/comment_detail.html'
+
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.text_comment = "Удалено администратором"
+        self.object.save()
+        contex = self.get_context_data(object=self.object)
+        return self.render_to_response(contex)
+
+class AnotherLoginView(LoginView):
+    template_name = "news/aut_str.html"
+
+class AnotherLogoutView(LogoutView):
+    template_name = "news/another_logout.html"
+    # next_page = '/'
