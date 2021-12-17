a9b6e59126d54f8a91539a9efe47ea057cb1a4d4
Max <dvdvdvd90@mail.ru>
2021-12-17 12:52:30 +0300

    В работе

--- 08_RegistrationAndAuthorization/djregistration/app_users/models.py
+++ 08_RegistrationAndAuthorization/djregistration/app_users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+class News(models.Model):
+    hedline_news = models.CharField(max_length=100)
+    text_news = models.TextField(verbose_name="Текст новости")
+    update_news = models.DateTimeField(auto_now=True)
+    created_news = models.DateTimeField(auto_now_add=True)
+    status = models.ForeignKey('Status_news', on_delete=models.CASCADE, verbose_name='Статус новости')
+
+
+class Status_news(models.Model):
+    str_status = models.CharField(max_length=50, verbose_name='Статус новости')
+
+
